8bcaa71a616c0383065ccae409c8fdd0415c6a9a
jay <jay@splitstreams.com>
2021-03-26 22:02:06 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements():
 
 setup(
     name='taxman',
-    version='0.2.0',
+    version='0.2.1',
     author='Jay Deiman',
     author_email='admin@splitstreams.com',
     description=(
