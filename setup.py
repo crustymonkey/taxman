d314a5a576ca40fc8b017bd5e94372de6cdf2fe1
Jay Deiman <jay@splitstreams.com>
2021-04-10 14:44:13 -0700

    Added speedtest-cli to the requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements():
 
 setup(
     name='taxman',
-    version='0.2.5',
+    version='0.2.6',
     author='Jay Deiman',
     author_email='admin@splitstreams.com',
     description=(
