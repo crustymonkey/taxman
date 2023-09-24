d63c75d2f88ff76a5401f5c0f00c5a8cadee4e30
Jay Deiman <jay@splitstreams.com>
2023-09-24 15:47:07 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements():
 
 setup(
     name='taxman',
-    version='0.3.11',
+    version='0.3.12',
     author='Jay Deiman',
     author_email='admin@splitstreams.com',
     description=(
